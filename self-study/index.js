01c95c3fb23a180872292312bf26a813cf50c491
Risa La Fountain <risa.garza@outlook.com>
2023-04-12 11:20:36 -0500

    first commit

---
+++ self-study/index.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("button");
+button.addEventListener ("click", myFunction); 
+
+var myFunction = function () {
+   var input = document.getElementById("input"); 
+   console.log (input.value)
+}
+
+
