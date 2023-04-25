fcbfcb9bc02c7c41ee24c507512db787897e5fed
Risa La Fountain <risa.garza@outlook.com>
2023-04-25 17:02:59 -0500

    challenge completed 4.25.23

---
+++ 53-string-methods-exercise/string-methods.js
@@ -0,0 +1,6 @@
+//1. write a function that takes a string as a parameter 
+function capilizeAndLowercase (str) {
+    return str.toUpperCase() + str.toLowerCase() 
+   }
+   
+   console.log (capilizeAndLowercase("HelLo")) 
